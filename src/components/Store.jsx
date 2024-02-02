d3f529c8e1fcab58f75df16eb1a248f31427cc94
Deepak Chowdry <113930530+DeepakChowdry-bit@users.noreply.github.com>
2024-02-02 22:40:56 +0530

    Update Store.jsx

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -10,37 +10,43 @@ const Store = () => {
                     <h3 className='font-bold uppercase text-sm'>New Arrivals</h3>
                 </div>
                 <div className='flex flex-wrap items-center justify-center w-full h-full gap-4'>
-                    <Link href={'/productpage'}>
-                        <div className='flex flex-col items-center justify-between shadow-md rounded-md overflow-hidden'>
-                        <Image src="/sofas/sofa (10).jpg" width={200} height={100} alt='' className='w-60 lg:w-64' />
-                        <div className='flex flex-col items-center justify-center w-90 h-12'>
-                            <h4 className='text-xs font-semibold uppercase tracking-wider'>Industrial</h4>
-                            <p className='text-xss font-semibold tracking-wide'>$299.00</p>
+                    <Link href={'/productpage'} className='w-9/12 lg:w-auto md:w-auto'>
+                        <div className='flex flex-col items-center justify-between shadow-md w-full lg:w-auto md:w-auto border border-black'>
+                            <Image src="/sofas/sofa (10).jpg" width={200} height={100} alt='' className='w-full lg:w-64 md:w-60' />
+                            <div className='flex flex-col items-center justify-center w-90 h-12'>
+                                <h4 className='text-xs font-semibold uppercase tracking-wider'>Industrial</h4>
+                                <p className='text-xss font-semibold tracking-wide'>$299.00</p>
+                            </div>
                         </div>
-                    </div>
                     </Link>
                     
-                    <div className='flex flex-col items-center justify-between shadow-md rounded-md overflow-hidden'>
-                        <Image src="/sofas/sofa (2).jpg" width={200} height={100} alt='' className='w-60 lg:w-64' />
-                        <div className='flex flex-col items-center justify-center w-90 h-12'>
-                            <h4 className='text-xs font-semibold uppercase tracking-wider'>Industrial</h4>
-                            <p className='text-xss font-semibold tracking-wide'>$299.00</p>
+                    <Link href={'/productpage'} className='w-9/12 lg:w-auto md:w-auto'>
+                        <div className='flex flex-col items-center justify-between shadow-md w-full lg:w-auto md:w-auto border border-black'>
+                            <Image src="/sofas/sofa (10).jpg" width={200} height={100} alt='' className='w-full lg:w-64 md:w-60' />
+                            <div className='flex flex-col items-center justify-center w-90 h-12'>
+                                <h4 className='text-xs font-semibold uppercase tracking-wider'>Industrial</h4>
+                                <p className='text-xss font-semibold tracking-wide'>$299.00</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className='flex flex-col items-center justify-between shadow-md rounded-md overflow-hidden'>
-                        <Image src="/sofas/sofa (3).jpg" width={200} height={100} alt='' className='w-60 lg:w-64' />
-                        <div className='flex flex-col items-center justify-center w-90 h-12'>
-                            <h4 className='text-xs font-semibold uppercase tracking-wider'>Industrial</h4>
-                            <p className='text-xss font-semibold tracking-wide'>$299.00</p>
+                    </Link>
+                    <Link href={'/productpage'} className='w-9/12 lg:w-auto md:w-auto'>
+                        <div className='flex flex-col items-center justify-between shadow-md w-full lg:w-auto md:w-auto border border-black'>
+                            <Image src="/sofas/sofa (10).jpg" width={200} height={100} alt='' className='w-full lg:w-64 md:w-60' />
+                            <div className='flex flex-col items-center justify-center w-90 h-12'>
+                                <h4 className='text-xs font-semibold uppercase tracking-wider'>Industrial</h4>
+                                <p className='text-xss font-semibold tracking-wide'>$299.00</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className='flex flex-col items-center justify-between shadow-md rounded-md overflow-hidden'>
-                        <Image src="/sofas/sofa (7).jpg" width={200} height={100} alt='' className='w-60 lg:w-64' />
-                        <div className='flex flex-col items-center justify-center w-90 h-12'>
-                            <h4 className='text-xs font-semibold uppercase tracking-wider'>Industrial</h4>
-                            <p className='text-xss font-semibold tracking-wide'>$299.00</p>
+                    </Link>
+                    <Link href={'/productpage'} className='w-9/12 lg:w-auto md:w-auto'>
+                        <div className='flex flex-col items-center justify-between shadow-md w-full lg:w-auto md:w-auto border border-black'>
+                            <Image src="/sofas/sofa (10).jpg" width={200} height={100} alt='' className='w-full lg:w-64 md:w-60' />
+                            <div className='flex flex-col items-center justify-center w-90 h-12'>
+                                <h4 className='text-xs font-semibold uppercase tracking-wider'>Industrial</h4>
+                                <p className='text-xss font-semibold tracking-wide'>$299.00</p>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
 
                 </div>
                 <button className='py-4 px-8 bg-zinc-950 text-white uppercase tracking-wider text-xs'>Shop All</button>
